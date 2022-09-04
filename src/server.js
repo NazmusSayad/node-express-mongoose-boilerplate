d91e2f2074e5a48cc22fe3b985ba87f7159043bb
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-09-04 08:08:57 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
+const coreUtils = require('./core')
+global.ReqError = coreUtils.ReqError
+global.catchAsync = coreUtils.catchAsync
 const mongoose = require('mongoose')
-global.ReqError = require('./core/req-error')
 const app = require('./app')
 
 process.env.PORT ||= 1000
 app.listen(process.env.PORT, () => {
-  console.log(`>>> App running on port "${process.env.PORT}"...`)
+  console.log('\x1b[32m%s\x1b[0m', `>>> App running on port "${process.env.PORT}"...`)
 })
 
 mongoose
   .connect(process.env.DB)
   .then(() => {
-    console.log('>>> MongoDB connected successfully...')
+    console.log('\x1b[32m%s\x1b[0m', '>>> MongoDB connected successfully...')
   })
   .catch(() => {
-    console.log('!!! MongoDB connection failed...')
-    console.log('!!! Server stopping...')
+    console.error('\x1b[31m%s\x1b[0m', '!!! MongoDB connection failed...')
+    console.error('\x1b[31m%s\x1b[0m', '!!! Server stopping...')
     process.exit()
   })
