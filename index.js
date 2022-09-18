d10c5aa49cb395807191b93834242d463b37f5e0
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-09-18 11:52:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 if (process.argv.at(-1) === '--NODE_ENV=development') {
   console.clear()
+  console.log('\x1b[0m')
   process.env.NODE_ENV = 'development'
   require('dotenv').config()
 } else process.env.NODE_ENV ||= 'production'
 
 require('./src/server')
+process.env.NODE_ENV === 'development' && console.log('\x1b[0m')
