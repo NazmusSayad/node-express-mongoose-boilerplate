76ddaa7af6e61a76a6644f13d5746c302e0521e0
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-11-26 09:22:38 +0000

    date

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 require('manual-node-env')
-console.log(
-  require('colors/safe').reset(
-    '---',
-    new Date().toLocaleString('en-US', {
-      dateStyle: 'full',
-      timeStyle: 'long',
-      hour12: true,
-    })
-  )
-)
+console.log(require('colors/safe').reset('---', new Date()))
 
 if (process.env.NODE_ENV === 'development') {
   console.clear()
